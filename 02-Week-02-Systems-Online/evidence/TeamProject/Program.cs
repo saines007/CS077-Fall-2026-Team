6627af5ec7e1a33ddafc4556c82b641bc1ae365c
saines007 <pascualsaines@gmail.com>
2026-09-04 13:31:33 -0700

    Testing time!

---
+++ 02-Week-02-Systems-Online/evidence/TeamProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject
+{
+    static string testExample = "This is test example of what you have to do";
+    static string testExample2 = "We will only be doing a sample of what we did in class.";
+    static string testExample3 = "Don't forget to close your containers and clean up your workspace.";
+    static string testExample4 = "I will not show how to code, but how to push directly to git from here.";
+    static string testExample5 = "Don't share my email please, thank you.";
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
